9580a6042ec628cbc6a4066ccf8671d5dff1b0ad
LaurenDarrimon <laurendarrimon@gmail.com>
2022-02-09 15:43:23 -0800

    build homepage routes for locations, products by location, one product, and login form

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const itemsRoutes = require('./items-routes');
 const commentsRoutes = require('./comments-routes');
+const loctionsRoutes = require('./locations-routes');
 
 router.use('/users', userRoutes);
 router.use('/items', itemsRoutes);
 router.use('/comments', commentsRoutes);
+router.use('/locations', locationsRoutes);
 
 module.exports = router;
