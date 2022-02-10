e1c0df8c22801eddc22e0d69910960fbbf88b2e1
David Chou <davidchou2009@gmail.com>
2022-02-09 16:31:13 -0800

    add Location Model

--- Models/Location.js
+++ Models/Location.js
@@ -1,2 +1,36 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const User = require('./User');
+
+class Location extends Model { }
+
+Location.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        city: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: User,
+                key: 'id'
+            }
+        }
+        
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'location',
+      }
+);
+module.exports = Location;
