80e04e04bd0177cd92690e16834089aac7077e66
LaurenDarrimon <laurendarrimon@gmail.com>
2022-02-11 11:03:06 -0800

    add index for all seed data files, add seed data and seeding functions  for items and comments

---
+++ seeds/index.js
@@ -0,0 +1,30 @@
+//import requreed seed files 
+
+const seedLocation = require('./locationData');
+const seedUsers = require('./userData');
+const seedItems = require('./itemData');
+const seedComments = require('./commentData');
+
+//connect to the database 
+const sequelize = require('../config/connection');
+
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
+  console.log('\n --- DATABASE SYNCED ----\n');
+  await seedLocation();
+  console.log('\n----- AUTHORS SEEDED ---\n');
+
+  await seedUsers();
+  console.log('\n--- ARTICLES SEEDED -----\n');
+
+  await seedItems();
+  console.log('\n----- COMMENTS SEEDED -----\n');
+
+  await seedComments();
+  console.log('\n----- COMMENTS SEEDED -----\n');
+
+  process.exit(0);
+};
+
+//function to run all the seed js files, filling our database tables with seed data
+seedAll();
